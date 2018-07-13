9a855a110b886e68fe27af953d5361b526860216
Ryan Birmingham <birm@rbirm.us>
2018-07-13 11:31:28 -0400

    layersdata population

--- js/init.js
+++ js/init.js
@@ -13,14 +13,14 @@ camic.loadImg()
 async function getLayers(){
   var mts = await camic.store.getMarktypes()
   var hms = await camic.store.getHeatmaps()
-  var layersData = []
+  var retData = []
   mts.forEach(async function(mt){
     // create a layer
     let layer = camic.layers.getLayer(mt.name)
     // get marks
     let marks = await camic.store.getMarks(mt.name)
-    let layerdata = {"id": mt.name, "name": mt.name, "typeId":1, "typeName": "Human Annotation"}
-    layersData.push(layerData);
+    let ld = {"id": mt.name, "name": mt.name, "typeId":1, "typeName": "Human Annotation"}
+    retData.push(ld);
     // put marks on layer
     marks.forEach(mark => {
       renderFeature(mt.name, mark, layer);
@@ -38,16 +38,18 @@ async function getLayers(){
     var size = viewer.world.getItemAt(0).getContentSize();
     let heat = simpleheat(dl, hm.height, hm.width, size.x, size.y)
     heat.data(hm.values).max(10000).draw()
-    let layerdata = {id: hm.name, name: hm.name, typeId:2, typeName: "Heatmap"}
-    layersData.push(layerData)
+    let ld = {id: hm.name, name: hm.name, typeId:2, typeName: "Heatmap"}
+    console.log(ld)
+    retData.push(ld)
+    console.log(retData)
   }.bind(this))
   // ensure all disabled
   //camic.layers.visibleLayers = new Set([]);
   // put together to layerdata
-  return layersData;
+  return retData;
 }
 var layersData, layerData
 camic.viewer.addHandler('open', ()=>{
-  layersData= getLayers()
+  getLayers().then(x=>layersData=x)
   layerData = layersData
 })
